e8f3118c10d0c1399370e52d1d1172386a807236
Dave Ankin <alexanderankin@gmail.com>
2018-08-12 04:22:15 -0400

    single variable memoization on middleware

--- middleware/index.js
+++ middleware/index.js
@@ -2,18 +2,25 @@ var qs = require('querystring');
 
 var knex = require('../db');
 
+var success = false;
+
 function checkInstall(req, res, next) {
-  // console.log(req.originalUrl.startsWith('/install'));
+  // assume db not dropped during server function
+  if (success) return next();
+
+  // allow for install to work before installed
   if (req.originalUrl.startsWith('/install')) {
     return next();
   }
 
+  // check if settings table is present
   knex.schema.hasTable('settings').asCallback(function (error, exists) {
     if (error) {
       res.redirect('/install?' + qs.stringify({ error: error.code }));
     }
 
     if (exists) {
+      success = true;
       next();
     } else {
       res.redirect('/install?' + qs.stringify({ error: 'No Table' }));
